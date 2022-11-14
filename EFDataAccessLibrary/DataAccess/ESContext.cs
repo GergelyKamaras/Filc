7b069126914b58b8c5ee790d175b5c4f19f81851
Dfulop98 <dobo.fulop@icloud.com>
2022-11-14 22:06:38 +0100

    Add: education system context(set models(tables) to database)

---
+++ EFDataAccessLibrary/DataAccess/ESContext.cs
@@ -0,0 +1,13 @@
+﻿using FilcDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FilcDataAccessLibrary.DataAccess
+{
+    public class ESContext : DbContext
+    {
+        public ESContext(DbContextOptions options) : base(options) { }
+        public DbSet<Teacher> Teacher { get; set; }
+
+    }
+}
