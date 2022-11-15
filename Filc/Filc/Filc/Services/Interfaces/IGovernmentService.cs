f82f19edf4afafc841887d805c693d7ef8999cfa
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-15 10:49:38 +0100

    Add: interface for government service

---
+++ Filc/Filc/Filc/Services/Interfaces/IGovernmentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Filc.Filc.Filc.Services.Interfaces
+{
+    public interface IGovernmentService
+    {
+        public void AddSchool(School school);
+        public void RemoveSchool(int id);
+        public List<School> ReturnAllSchools();
+    }
+}
