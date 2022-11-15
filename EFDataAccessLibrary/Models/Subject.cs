ab0bc280525742deca342eb8eac6292a34f16014
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 11:23:18 +0100

    add: subject table model

---
+++ EFDataAccessLibrary/Models/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilcDataAccessLibrary.Models
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string Title { get; set; }
+        public int AmountOfStudents { get; set; }
+
+    }
+}
