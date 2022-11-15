edfffe7f6ecca4859b620eb4c044255d9e3671e2
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 11:23:01 +0100

    Fix: move these files to separate folder

---
+++ EFDataAccessLibrary/Models/ModelInterfaces/ISchoolMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Models.ModelInterfaces
+{
+    public interface ISchoolMember : IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Address { get; set; }
+    }
+}
