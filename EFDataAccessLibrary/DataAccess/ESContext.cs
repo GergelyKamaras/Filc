149f4437c9c61da0a764c343de0ac46fc30b477c
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 14:40:36 +0100

    Add: few more table

--- EFDataAccessLibrary/DataAccess/ESContext.cs
+++ EFDataAccessLibrary/DataAccess/ESContext.cs
@@ -7,6 +7,10 @@ namespace EFDataAccessLibrary.DataAccess
     public class ESContext : DbContext
     {
         public ESContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<School> School { get; set; }
+        public DbSet<SchoolClass> SchoolClasses { get; set; }
+        public DbSet<SchoolAdmin> SchoolAdmin { get; set; }
         public DbSet<Teacher> Teacher { get; set; }
         public DbSet<Subject> Subject { get; set; }
         public DbSet<Student> Student { get; set; }
