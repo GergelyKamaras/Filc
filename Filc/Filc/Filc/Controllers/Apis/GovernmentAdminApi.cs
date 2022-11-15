ce62bf14c2409e83078972fa3031cb90ae7f2e2d
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-15 13:08:26 +0100

    Add: GovernmentAdminApi controller with school related methods

---
+++ Filc/Filc/Filc/Controllers/Apis/GovernmentAdminApi.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filc.Filc.Filc.Controllers.Apis
+{
+    // TODO restrict Endpoint access to GovernmentAdmins
+    [Route("/api/gov")]
+    public class GovernmentAdminApi : Controller
+    {
+        [HttpGet]
+        [Route("school")]
+        public School GetSchool(int schoolId)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("school")]
+        public void AddSchool(School school)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("school")]
+        public void UpdateSchool(School school)
+        {
+
+        }
+
+        [HttpDelete]
+        [Route("school")]
+        public void DeleteSchool(int schoolId)
+        {
+
+        }
+    }
+}
