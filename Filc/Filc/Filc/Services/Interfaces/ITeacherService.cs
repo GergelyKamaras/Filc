c15267e778d39ee1102d9311926eb6d5510542b7
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-15 11:28:55 +0100

    Add: interface for teacher service

---
+++ Filc/Filc/Filc/Services/Interfaces/ITeacherService.cs
@@ -0,0 +1,8 @@
+﻿namespace Filc.Filc.Filc.Services.Interfaces
+{
+    public interface ITeacherService
+    {
+        public Mark AddMark(int studentId);
+        public void RegisterAbsence();
+    }
+}
