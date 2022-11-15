00da54e37a91ac379a3d988319a6b07b2d609d93
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 12:54:58 +0100

    Add: lesson model

---
+++ EFDataAccessLibrary/Models/Lesson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilcDataAccessLibrary.Models
+{
+    public class Lesson
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public Subject Subject { get; set; }
+        public List<Student> students { get; set; }
+        public List<Teacher> Teachers { get; set; }
+
+    }
+}
