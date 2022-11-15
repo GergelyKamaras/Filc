579e30fc4c2a40b3649e8e81f934823ea2ca9020
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-15 11:20:09 +0100

    Add: interface for schoolservice class, add new method signature to government service

---
+++ Filc/Filc/Filc/Services/Interfaces/ISchoolService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+
+namespace Filc.Filc.Filc.Services.Interfaces
+{
+    // Services should be accessed only by SchoolAdmins
+    public interface ISchoolService
+    {
+        public void RegisterTeacher(Teacher teacher, int schoolId);
+        public void RegisterStudent(Student student, int schoolId);
+        public void DeleteUser(UserRole userRole, int id);
+        public void UpdateUser(IUser user);
+        public void UpdateLesson(int lessonId);
+        public void AddStudentToClass(int StudentId, int ClassId);
+        public void CreateLesson();
+    }
+}
