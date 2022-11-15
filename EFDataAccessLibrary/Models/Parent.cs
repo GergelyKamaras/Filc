071abd8c1b188239e8a6b55ae7a378f457e82ab4
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 13:04:31 +0100

    Add: parent model

---
+++ EFDataAccessLibrary/Models/Parent.cs
@@ -0,0 +1,18 @@
+﻿using EFDataAccessLibrary.Models.ModelInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Parent : IUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+        public List<Student> Childs { get; set; }
+    }
+}
