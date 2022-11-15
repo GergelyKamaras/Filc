e1408bfcb7c6aa8e135609155474586f0b3ac1f8
Dfulop98 <dobo.fulop@icloud.com>
2022-11-15 12:55:09 +0100

    Add: student model

---
+++ EFDataAccessLibrary/Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using EFDataAccessLibrary.Models.ModelInterfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilcDataAccessLibrary.Models
+{
+    public class Student : ISchoolMember
+    {
+        [Required]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(40)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string BirthDate { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        //TODO: encrypt password
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string UserRole { get; set; }
+
+        public List<Lesson> Lessons { get; set; }
+    }
+}
