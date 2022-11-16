6d735e08feb29c57673f484ef20e977397ede870
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:41:29 +0100

    Add: update to ISchoolService interface

--- Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolService.cs
@@ -4,10 +4,10 @@ namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
     public interface ISchoolService
     {
+        public School GetSchool(int id);
+        public List<School> GetAllSchools();
         public void AddSchool(School school);
         public void RemoveSchool(int id);
         public void UpdateSchool(School school);
-        public School GetSchool(int id);
-        public List<School> ReturnAllSchools();
     }
 }
