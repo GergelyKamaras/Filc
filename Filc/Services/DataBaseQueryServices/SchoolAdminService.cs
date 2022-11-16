250031c23d467b66284401c9f26b5abb46716c57
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 16:38:11 +0100

    Fix: missing db change save

--- Filc/Services/DataBaseQueryServices/SchoolAdminService.cs
+++ Filc/Services/DataBaseQueryServices/SchoolAdminService.cs
@@ -14,6 +14,7 @@ namespace Filc.Services.DataBaseQueryServices
         public void AddSchoolAdmin(SchoolAdmin schoolAdmin)
         {
             _db.SchoolAdmin.Add(schoolAdmin);
+            _db.SaveChanges();
         }
         public SchoolAdmin GetSchoolAdmin(int schoolAdminId)
         {
