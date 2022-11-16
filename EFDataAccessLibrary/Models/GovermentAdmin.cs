4741ad4e364c43c73cdf537d261a784b39a251a9
Dfulop98 <dobo.fulop@icloud.com>
2022-11-16 11:08:47 +0100

    Add: annotations

--- EFDataAccessLibrary/Models/GovermentAdmin.cs
+++ EFDataAccessLibrary/Models/GovermentAdmin.cs
@@ -1,6 +1,7 @@
 ﻿using EFDataAccessLibrary.Models.ModelInterfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace EFDataAccessLibrary.Models
     public class GovermentAdmin : IUser
     {
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(200)]
         public string Email { get; set; }
         public string Password { get; set; }
         public string UserRole { get; set; }
