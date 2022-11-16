1858def9b91aebd56f53bae914af48ed49308f2a
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 16:23:34 +0100

    Fix: typo

--- Filc/Services/Interfaces/RoleBasedInterfaces/ParentRole/IParentRoleMarkService.cs
+++ Filc/Services/Interfaces/RoleBasedInterfaces/ParentRole/IParentRoleMarkService.cs
@@ -4,7 +4,7 @@ namespace Filc.Services.Interfaces.RoleBasedInterfaces.ParentRole
 {
     public interface IParentRoleMarkService
     {
-        public Mark GeMark(int id);
+        public Mark GetMark(int id);
         public List<Mark> GetMarksByStudent(int studentId);
         public List<Mark> GetMarksByLessonByStudent(int studentId, int lessonId);
     }
