42cb05116d483767ab425c72c1c04d686d1924c7
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 13:35:43 +0100

    Add: method signatures for student role specific interfaces

--- Filc/Services/Interfaces/RoleBasedInterfaces/StudentRole/IStudentRoleStudentService.cs
+++ Filc/Services/Interfaces/RoleBasedInterfaces/StudentRole/IStudentRoleStudentService.cs
@@ -1,6 +1,10 @@
-﻿namespace Filc.Services.Interfaces.RoleBasedInterfaces.StudentRole
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.RoleBasedInterfaces.StudentRole
 {
     public interface IStudentRoleStudentService
     {
+        public Student GetStudent(int id);
+        public void UpdateStudent(Student student);
     }
 }
