634760e80fc52fd767dceb585c0bb5cccff9129a
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:34:04 +0100

    Add: ILessonService interface

--- Filc/Services/Interfaces/EntityBasedInterfaces/ILessonService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/ILessonService.cs
@@ -1,7 +1,12 @@
-﻿namespace Filc.Services.Interfaces.EntityBasedInterfaces
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
     public interface ILessonService
     {
-
+        public Lesson GetLessonById(int id);
+        public void AddLesson(Lesson lesson);
+        public void UpdateLesson(Lesson lesson);
+        public void DeleteLesson(int id);
     }
 }
