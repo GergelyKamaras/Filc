d93c569e88b047282d8f7d440b7847fe919f5b40
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-16 10:33:07 +0100

    Feat: Add GovAdminController for view

---
+++ Filc/Controllers/UserControllers/GovAdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filc.Controllers.UserControllers
+{
+    [Route("/govadmin")]
+    public class GovAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
