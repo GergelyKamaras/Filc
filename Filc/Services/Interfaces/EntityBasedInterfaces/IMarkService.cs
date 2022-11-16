3caaf497d51879acfd6a69429d6bc40acddc6a03
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:36:43 +0100

    Add: IMarkService interface

--- Filc/Services/Interfaces/EntityBasedInterfaces/IMarkService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/IMarkService.cs
@@ -1,6 +1,15 @@
-﻿namespace Filc.Services.Interfaces.EntityBasedInterfaces
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
     public interface IMarkService
     {
+        public Mark GeMark(int id);
+        public void AddMark(Mark mark);
+        public void UpdateMark(Mark mark);
+        public void DeleteMark(int id);
+        public List<Mark> GetMarksByStudent(int studentId);
+        public List<Mark> GetMarkByLesson(int lessonId);
+        public List<Mark> GetMarksByLessonByStudent(int studentId, int lessonId);
     }
 }
