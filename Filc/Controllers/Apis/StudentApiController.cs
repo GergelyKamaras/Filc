f96be623866b8bb34091715814ebdc3c274a578e
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 13:41:28 +0100

    Add: DI to StudentApiController

---
+++ Filc/Controllers/Apis/StudentApiController.cs
@@ -0,0 +1,25 @@
+﻿using Filc.Services.Interfaces.RoleBasedInterfaces.StudentRole;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filc.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentApiController : ControllerBase
+    {
+        
+        private IStudentRoleLessonService _lessonService;
+        private IStudentRoleMarkService _markService;
+        private IStudentRoleSchoolService _schoolService;
+        private IStudentRoleStudentService _studentService;
+        public StudentApiController(IStudentRoleLessonService lessonService, IStudentRoleMarkService markService,
+            IStudentRoleSchoolService schoolService, IStudentRoleStudentService studentService)
+        {
+            _studentService = studentService;
+            _lessonService = lessonService;
+            _markService = markService;
+            _schoolService = schoolService;
+        }
+    }
+}
