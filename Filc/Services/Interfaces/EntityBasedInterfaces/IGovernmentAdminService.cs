f2e81d924bc313da33a3025c516279bfab18d8de
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:31:45 +0100

    Add: IGovernmentAdminService interface, fix typo in GovernmentAdmin modell class

--- Filc/Services/Interfaces/EntityBasedInterfaces/IGovernmentAdminService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/IGovernmentAdminService.cs
@@ -1,6 +1,12 @@
-﻿namespace Filc.Services.New_intefaces
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
-    public class IGovernmentAdminService
+    public interface IGovernmentAdminService
     {
+        public GovernmentAdmin GetGovernmentAdmin(int id);
+        public void AddGovernmentAdmin(GovernmentAdmin governmentAdmin);
+        public void RemoveGovernmentAdmin(int id);
+        public void UpdateGovernmentAdmin(GovernmentAdmin governmentAdmin);
     }
 }
