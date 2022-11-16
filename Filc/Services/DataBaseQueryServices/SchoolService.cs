407524a8adde369a46ac5ade30fe86eb643f6243
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 16:38:23 +0100

    Add: school service implementation

--- Filc/Services/DataBaseQueryServices/SchoolService.cs
+++ Filc/Services/DataBaseQueryServices/SchoolService.cs
@@ -1,6 +1,42 @@
-﻿namespace Filc.Services.DataBaseQueryServices
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Filc.Services.Interfaces.EntityBasedInterfaces;
+
+namespace Filc.Services.DataBaseQueryServices
 {
-    public class SchoolService
+    public class SchoolService : ISchoolService
     {
+        private ESContext _db;
+        public SchoolService(ESContext esContext)
+        {
+            _db = esContext;
+        }
+
+        public void AddSchool(School school)
+        {
+            _db.School.Add(school);
+            _db.SaveChanges();
+        }
+
+        public School GetSchool(int id)
+        {
+            return _db.School.First(x => x.Id == id);
+        }
+        public List<School> GetAllSchools()
+        {
+            return _db.School.ToList();
+        }
+
+        public void RemoveSchool(int id)
+        {
+            _db.School.Remove(_db.School.First(school => school.Id == id));
+            _db.SaveChanges();
+        }
+
+        public void UpdateSchool(School school)
+        {
+            _db.School.Update(school);
+            _db.SaveChanges();
+        }
     }
 }
