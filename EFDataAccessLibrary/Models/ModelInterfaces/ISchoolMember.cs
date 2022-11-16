50272c78293eae9900bccfcff0eb8996f0110b40
Dfulop98 <dobo.fulop@icloud.com>
2022-11-16 15:31:10 +0100

    Fix: birth date change to dateTime type

--- EFDataAccessLibrary/Models/ModelInterfaces/ISchoolMember.cs
+++ EFDataAccessLibrary/Models/ModelInterfaces/ISchoolMember.cs
@@ -10,7 +10,7 @@ namespace EFDataAccessLibrary.Models.ModelInterfaces
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Address { get; set; }
         public School School { get; set; }
     }
