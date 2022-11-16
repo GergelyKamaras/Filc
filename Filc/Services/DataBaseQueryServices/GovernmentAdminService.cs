9873c9f1e1a38047b0ae57f786364b9f1d77199f
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 14:47:34 +0100

    Add: GovernmentAdminService query implementations

---
+++ Filc/Services/DataBaseQueryServices/GovernmentAdminService.cs
@@ -0,0 +1,42 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Filc.Services.Interfaces.EntityBasedInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filc.Services.DataBaseQueryServices
+{
+    public class GovernmentAdminService : IGovernmentAdminService
+    {
+        private ESContext _db;
+        public GovernmentAdminService(ESContext esContext)
+        {
+            _db = esContext;
+        }
+        public GovernmentAdmin GetGovernmentAdmin(int id)
+        {
+            return _db.GovernmentAdmin.First(x => x.Id == id);
+        }
+
+        public void AddGovernmentAdmin(GovernmentAdmin governmentAdmin)
+        {
+            List<GovernmentAdmin> governmentAdmins = _db.GovernmentAdmin.ToList();
+            governmentAdmins.Add(governmentAdmin);
+            _db.SaveChanges();
+        }
+
+        public void RemoveGovernmentAdmin(int id)
+        {
+            List<GovernmentAdmin> governmentAdmins = _db.GovernmentAdmin.ToList();
+            var admin = governmentAdmins.First(x => x.Id == id);
+            governmentAdmins.Remove(admin);
+            _db.SaveChanges();
+        }
+        public void UpdateGovernmentAdmin(GovernmentAdmin governmentAdmin)
+        {
+            List<GovernmentAdmin> governmentAdmins = _db.GovernmentAdmin.ToList();
+            var oldAdmin = governmentAdmins.First(x => x.Id == governmentAdmin.Id);
+            oldAdmin = governmentAdmin;
+            _db.SaveChanges();
+        }
+    }
+}
