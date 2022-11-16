1752752a5cbca290e1361724a2e32c43c5d3e6e9
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:38:40 +0100

    Add: IParentService interface

--- Filc/Services/Interfaces/EntityBasedInterfaces/IParentService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/IParentService.cs
@@ -1,6 +1,12 @@
-﻿namespace Filc.Services.Interfaces.EntityBasedInterfaces
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
     public interface IParentService
     {
+        public Parent GetParent(int id);
+        public void AddParent(Parent parent);
+        public void UpdateParent(Parent parent);
+        public void DeleteParent(int id);
     }
 }
