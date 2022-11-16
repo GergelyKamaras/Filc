92ee7fad32b3ba1a885ad25d33fdf11e5175b233
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 10:26:53 +0100

    Rafactor: Move interfaces between folders, delete unused files

--- Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolService.cs
@@ -1,12 +1,13 @@
 ﻿using EFDataAccessLibrary.Models;
 
-namespace Filc.Services.Interfaces
+namespace Filc.Services.New_intefaces.EntityBasedInterfaces
 {
-    public interface IGovernmentService
+    public interface ISchoolService
     {
         public void AddSchool(School school);
         public void RemoveSchool(int id);
+        public void UpdateSchool(School school);
+        public School GetSchool(int id);
         public List<School> ReturnAllSchools();
-        public void AddSchoolAdmin(int schoolId, SchoolAdmin schoolAdmin);
     }
 }
