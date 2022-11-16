dd057d0766d99d5b6bd061f5d387af36e11ed3f0
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-16 10:28:49 +0100

    Feat: Add SchoolAdminController for SchoolAdmin's view

---
+++ Filc/Controllers/UserControllers/SchoolAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filc.Controllers.UserControllers
+{
+    public class SchoolAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
