7e6935f0679ad2a61f29bced2c0a39f544021988
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-16 11:43:41 +0100

    Refactor: delete earlier role specific interfaces, start moving the method declarations to smaller sub-interfaces

---
+++ Filc/Services/Interfaces/RoleBasedInterfaces/ParentRole/IParentRoleSchoolService.cs
@@ -0,0 +1,15 @@
+﻿using EFDataAccessLibrary.Models;
+
+namespace Filc.Services.Interfaces.RoleBasedInterfaces.ParentRole
+{
+    public interface IParentRoleSchoolService
+    {
+        public School GetSchool(int id);
+        public Parent GetParent(int id);
+        public Student GetStudent(int id);
+        public void UpdateParent(Parent parent);
+        public void UpdateStudent(Student student);
+        public List<Mark> GetMarksByStudent(int studentId);
+        public List<Mark> GetMarksByLessonByStudent(int studentId, int lessonId);
+    }
+}
