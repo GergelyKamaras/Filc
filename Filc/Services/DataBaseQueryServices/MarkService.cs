db972403ca7ce0a1f2243486bbd10955bbc57742
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-17 14:35:00 +0100

    Add: update MarkService with connected object retrieval

--- Filc/Services/DataBaseQueryServices/MarkService.cs
+++ Filc/Services/DataBaseQueryServices/MarkService.cs
@@ -1,6 +1,7 @@
 ﻿using EFDataAccessLibrary.DataAccess;
 using EFDataAccessLibrary.Models;
 using Filc.Services.Interfaces.EntityBasedInterfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Filc.Services.DataBaseQueryServices
 {
@@ -13,20 +14,22 @@ namespace Filc.Services.DataBaseQueryServices
         }
         public Mark GetMark(int id)
         {
-            return _db.Mark.First(x => x.Id == id);
+            return _db.Mark.Include(mark => mark.Lesson)
+                .Include(mark => mark.Student)
+                .Include(mark => mark.Teacher)
+                .Include(mark => mark.Subject)
+                .First(x => x.Id == id);
         }
         public void AddMark(Mark mark)
         {
             _db.Mark.Add(mark);
             _db.SaveChanges();
         }
-
         public void UpdateMark(Mark mark)
         {
             _db.Mark.Update(mark);
             _db.SaveChanges();
         }
-
         public void DeleteMark(int id)
         {
             _db.Mark.Remove(_db.Mark.First(mark => mark.Id == id));
@@ -34,14 +37,19 @@ namespace Filc.Services.DataBaseQueryServices
         }
         public List<Mark> GetMarksByStudent(int studentId)
         {
-            return _db.Mark.Where(mark => mark.Student.Id == studentId).ToList();
+            return _db.Mark.Include(mark => mark.Lesson)
+                .Include(mark => mark.Student)
+                .Include(mark => mark.Teacher)
+                .Include(mark => mark.Subject)
+                .Where(mark => mark.Student.Id == studentId).ToList();
         }
-
         public List<Mark> GetMarkByLesson(int lessonId)
         {
-            return _db.Mark.Where(mark => mark.Lesson.Id == lessonId).ToList();
+            return _db.Mark.Where(mark => mark.Lesson.Id == lessonId).Include(mark => mark.Lesson)
+                .Include(mark => mark.Student)
+                .Include(mark => mark.Teacher)
+                .Include(mark => mark.Subject).ToList();
         }
-
         public List<Mark> GetMarksByLessonByStudent(int studentId, int lessonId)
         {
             return GetMarkByLesson(lessonId).Where(mark => mark.Student.Id == studentId).ToList();
