142458f88e4959f17d4ab83ee10b1df8fa99a238
Dfulop98 <dobo.fulop@icloud.com>
2022-11-17 14:01:04 +0100

    Add: login and logout phrase to controller

--- Filc/Controllers/AccountController.cs
+++ Filc/Controllers/AccountController.cs
@@ -13,6 +13,14 @@ namespace Filc.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -38,6 +46,30 @@ namespace Filc.Controllers
                 }
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                
+            }
+            return View(model);
+        
         }
 
     }
