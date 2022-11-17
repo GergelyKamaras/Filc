5d059fd7d0ab667e31975e6a5081b63e4888b33f
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-17 14:28:18 +0100

    Add: update governmentadmin query service and inteface

--- Filc/Services/Interfaces/EntityBasedInterfaces/IGovernmentAdminService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/IGovernmentAdminService.cs
@@ -6,7 +6,7 @@ namespace Filc.Services.Interfaces.EntityBasedInterfaces
     {
         public GovernmentAdmin GetGovernmentAdmin(int id);
         public List<GovernmentAdmin> GetAllGovernmentAdmins();
-        public void AddGovernmentAdmin(GovernmentAdmin governmentAdmin);
+        public void AddGovernmentAdmin(GovernmentAdmin governmentAdmin, string email);
         public void RemoveGovernmentAdmin(int id);
         public void UpdateGovernmentAdmin(GovernmentAdmin governmentAdmin);
     }
