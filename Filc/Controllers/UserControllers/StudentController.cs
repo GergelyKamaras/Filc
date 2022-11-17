2c39a192795b825b915bdac9aeca92d86bcfdcf0
tothhajnalka17 <tothhajnalka17@gmail.com>
2022-11-17 21:45:46 +0100

    Feat: Add login recognition by injecting UserManager, SignInManager, RoleManager to StudentController
    It ended up being unused because we couldn't access role-specific info like classes or grades yet (it hasn't been created)

--- Filc/Controllers/UserControllers/StudentController.cs
+++ Filc/Controllers/UserControllers/StudentController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filc.Controllers.UserControllers
 {
-    [Route("/nebulo")]
     public class StudentController : Controller
     {
-        
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public StudentController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
