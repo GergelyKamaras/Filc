215e735a6f6553176f68646234e12059ddec2153
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-28 13:17:18 +0100

    Refactor: remove method signature duplicates that are already available due to inheritance

--- Filc/Services/Interfaces/EntityBasedInterfaces/ILessonService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/ILessonService.cs
@@ -7,9 +7,6 @@ namespace Filc.Services.Interfaces.EntityBasedInterfaces
 {
     public interface ILessonService : IParentRoleLessonService, IStudentRoleLessonService, ITeacherRoleLessonService
     {
-        public Lesson GetLessonById(int id);
-        public List<Lesson> GetLessonByStudentId(int id);
-        public List<Lesson> GetLessonsByTeacher(int id);
         public void AddLesson(Lesson lesson);
         public void UpdateLesson(Lesson lesson);
         public void DeleteLesson(int id);
--- Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolAdminService.cs
+++ Filc/Services/Interfaces/EntityBasedInterfaces/ISchoolAdminService.cs
@@ -6,8 +6,6 @@ namespace Filc.Services.Interfaces.EntityBasedInterfaces
     public interface ISchoolAdminService : ISchoolAdminRoleSchoolAdminService
     {
         public void AddSchoolAdmin(SchoolAdmin schoolAdmin, string email);
-        public SchoolAdmin GetSchoolAdmin(int schoolAdminId);
-        public void UpdateSchoolAdmin(SchoolAdmin schoolAdmin);
         public void DeleteSchoolAdmin(int schoolAdminId);
     }
 }
