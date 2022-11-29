23990e8303e55bda94eca6b0da913f6b4ceb2705
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2022-11-29 13:55:07 +0100

    Feat: Add Registration Fetch
    By Zoárd

---
+++ Filc/ClientApp/src/components/controllers/RegistrationFetch.js
@@ -0,0 +1,11 @@
+﻿const RegistrationFetch = async (data) => {
+    const response = await fetch('/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+      body: JSON.stringify(data)
+  })
+}
+
+export default RegistrationFetch;
