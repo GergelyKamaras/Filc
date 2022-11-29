6e369bfd31969f4c4b81209b05891d3e81944afb
Gudmon <gudmonmarcell@gmail.com>
2022-11-29 11:55:02 +0100

    Add: AddSchoolAdmin method

--- Filc/Services/Interfaces/RoleBasedInterfacesForApis/SchoolAdminRole/ISchoolAdminServiceForSchoolAdminRole.cs
+++ Filc/Services/Interfaces/RoleBasedInterfacesForApis/SchoolAdminRole/ISchoolAdminServiceForSchoolAdminRole.cs
@@ -4,7 +4,7 @@ namespace Filc.Services.Interfaces.RoleBasedInterfaces.SchoolAdminRole
 {
     public interface ISchoolAdminServiceForSchoolAdminRole
     {
-        public void AddSchoolAdmin(SchoolAdmin schoolAdmin, string email);
+        public void AddSchoolAdmin(SchoolAdmin schoolAdmin);
         public List<SchoolAdmin> GetAllSchoolAdminsBySchool(int schoolId);
         public SchoolAdmin GetSchoolAdminById(int schoolAdminId);
         public void UpdateSchoolAdmin(SchoolAdmin schoolAdmin);
