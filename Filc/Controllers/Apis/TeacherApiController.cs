209c8237d309f871d5f04f7b1df41532b6f9864c
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-29 13:09:51 +0100

    Add: Teacher api controller endpoints

--- Filc/Controllers/Apis/TeacherApiController.cs
+++ Filc/Controllers/Apis/TeacherApiController.cs
@@ -1,4 +1,5 @@
-﻿using Filc.Services.Interfaces.RoleBasedInterfacesForApis.FullAccess;
+﻿using EFDataAccessLibrary.Models;
+using Filc.Services.Interfaces.RoleBasedInterfacesForApis.FullAccess;
 using Filc.Services.Interfaces.RoleBasedInterfacesForApis.TeacherRole;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,7 +8,7 @@ namespace Filc.Controllers.Apis
 {
     [Authorize]
     [ApiController]
-    [Route("api/teacher")]
+    [Route("api/teachers")]
     public class TeacherApiController : ControllerBase
     {
 
@@ -26,6 +27,101 @@ namespace Filc.Controllers.Apis
             _schoolService = teacherRoleSchoolService;
             _studentService = teacherRoleStudentService;
             _teacherService = teacherRoleTeacherService;
+        }
+
+        // Schools
+        [HttpGet]
+        [Route("schools/{id}")]
+        public School GetSchool(int id)
+        {
+            return _schoolService.GetSchool(id);
+        }
+
+        // Teachers
+        [HttpGet]
+        [Route("{id}")]
+        public Teacher GetTeacher(int id)
+        {
+            return _teacherService.GetTeacher(id);
+        }
+
+        [HttpPut]
+        public void UpdateTeacher([FromBody] Teacher teacher)
+        {
+            _teacherService.UpdateTeacher(teacher);
+        }
+
+        // Lessons
+        [HttpGet]
+        [Route("lessons/{id}")]
+        public Lesson GetLesson(int id)
+        {
+            return _lessonService.GetLessonById(id);
+        }
+
+        [HttpGet]
+        [Route("lessons/students/{id}")]
+        public List<Lesson> GetLessonsByStudent(int id)
+        {
+            return _lessonService.GetLessonByStudentId(id);
+        }
+
+        [HttpGet]
+        [Route("lessons/teachers/{id}")]
+        public List<Lesson> GetLessonsByTeacher(int id)
+        {
+            return _lessonService.GetLessonsByTeacher(id);
+        }
+
+        // Marks
+        [HttpGet]
+        [Route("marks/{id}")]
+        public Mark GetMark(int id)
+        {
+            return _markService.GetMark(id);
+        }
+
+        [HttpGet]
+        [Route("marks/student/{id}")]
+        public List<Mark> GetMarksByStudent(int id)
+        {
+            return _markService.GetMarksByStudent(id);
+        }
+
+        [HttpGet]
+        [Route("marks/lesson/{id}")]
+        public List<Mark> GetMarksByLesson(int id)
+        {
+            return _markService.GetMarkByLesson(id);
+        }
+
+        [HttpPost]
+        [Route("marks")]
+        public void AddMark([FromBody] Mark mark)
+        {
+            _markService.AddMark(mark);
+        }
+
+        [HttpPut]
+        [Route("marks")]
+        public void UpdateMark([FromBody] Mark mark)
+        {
+            _markService.UpdateMark(mark);
+        }
+
+        [HttpDelete]
+        [Route("marks/{id}")]
+        public void DeleteMark(int id)
+        {
+            _markService.DeleteMark(id);
+        }
+
+        // Students
+        [HttpGet]
+        [Route("students/{id}")]
+        public Student GetStudent(int id)
+        {
+            return _studentService.GetStudent(id);
         }
     }
 }
