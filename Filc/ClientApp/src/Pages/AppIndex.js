4f63dd0d1dcc5a5f387a40de1c6a909e493cfd76
Dfulop98 <dobo.fulop@icloud.com>
2022-11-29 19:17:40 +0100

    Add: index page react component

---
+++ Filc/ClientApp/src/Pages/AppIndex.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const IndexPage = () => {
+    return (
+        <>
+        <div>
+            sajt
+
+        </div>
+        </>
+    );
+};
+
+export default IndexPage;
