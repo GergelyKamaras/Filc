fce9f37452ae1c0ecb7a1f2655446e14b1d7be88
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-30 11:39:32 +0100

    Feat: Add RequireAuth for getting data from local storage and redirecting

---
+++ Filc/ClientApp/src/components/RequireAuth.js
@@ -0,0 +1,13 @@
+﻿import { useLocation, Navigate, Outlet } from 'react-router-dom';
+
+const RequireAuth = ({allowedRoles}) => {
+  const location = useLocation();
+
+  return (
+    localStorage?.token?.claims[1]?.find(role => allowedRoles.includes(role))
+      ? <Outlet />
+      : localStorage?.token
+        ? <Navigate to="*" state={{ from: location }} replace />
+        : <Navigate to="/" state={{ from: location }} replace />
+  );
+}
