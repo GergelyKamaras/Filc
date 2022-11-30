dbd40876bf3429c360fc738ea30e95114418cdaa
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-30 13:15:26 +0100

    Feat: Add Redirecting to Unauthorized page

--- Filc/ClientApp/src/components/RequireAuth.js
+++ Filc/ClientApp/src/components/RequireAuth.js
@@ -7,7 +7,7 @@ const RequireAuth = ({allowedRoles}) => {
     localStorage?.token?.claims[1]?.find(role => allowedRoles.includes(role))
       ? <Outlet />
       : localStorage?.token
-        ? <Navigate to="*" state={{ from: location }} replace />
+        ? <Navigate to="/Unauthorized" state={{ from: location }} replace />
         : <Navigate to="/" state={{ from: location }} replace />
   );
 }
