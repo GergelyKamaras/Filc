89d69f53a1e94570081baac8da928da905ed9ca2
Dfulop98 <dobo.fulop@icloud.com>
2022-11-30 16:04:05 +0100

    Add: service method for get salt

--- Filc/Services/Interfaces/RoleBasedInterfacesForApis/FullAccess/IUserServiceFullAccess.cs
+++ Filc/Services/Interfaces/RoleBasedInterfacesForApis/FullAccess/IUserServiceFullAccess.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Filc.Services.Interfaces.RoleBasedInterfacesForApis.FullAccess
 {
     public interface IUserServiceFullAccess
     {
-        public List<IdentityUser> GetAllUsers();
-        public IdentityUser GetUserById(string id);
-        public IdentityUser GetUserByEmail(string email);
-        public void UpdateUser(IdentityUser user);
+        public List<ApplicationUser> GetAllUsers();
+        public ApplicationUser GetUserById(string id);
+        public ApplicationUser GetUserByEmail(string email);
+        public string GetSaltByEmail(string email);
+        
+        public void UpdateUser(ApplicationUser user);
         public void DeleteUser(string id);
     }
 }
