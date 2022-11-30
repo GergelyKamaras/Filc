70b5ac839b4ca66f71c7ee8dbec9b8b18b7c6666
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-30 15:09:31 +0100

    Add: constructor for lesson view model for school

--- Filc/Models/EntityViewModels/School/LessonViewModelForSchool.cs
+++ Filc/Models/EntityViewModels/School/LessonViewModelForSchool.cs
@@ -8,6 +8,11 @@ namespace Filc.Models.EntityViewModels.School
         public int Id { get; set; }
         public string Name { get; set; }
         public Subject Subject { get; set; }
-
+        public LessonViewModelForSchool(Lesson lesson)
+        {
+            Id = lesson.Id;
+            Name = lesson.Name;
+            Subject = lesson.Subject;
+        }
     }
 }
