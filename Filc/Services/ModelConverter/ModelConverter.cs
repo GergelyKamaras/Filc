825f0cb11a2b2c6524812131b84f81b7943507ff
Gudmon <gudmonmarcell@gmail.com>
2022-11-30 15:08:56 +0100

    Add: ModelConverter file, convert to SchoolAdminViewModel

---
+++ Filc/Services/ModelConverter/ModelConverter.cs
@@ -0,0 +1,21 @@
+﻿using EFDataAccessLibrary.Models;
+using Filc.Models.EntityViewModels.SchoolAdmin;
+
+namespace Filc.Services.ModelConverter
+{
+    public static class ModelConverter
+    {
+        public static List<SchoolAdminViewModel> MapSchoolAdminToSchoolAdminViewModel(List<SchoolAdmin> schoolAdmin)
+        {
+            List<SchoolAdminViewModel> returnList = new List<SchoolAdminViewModel>();
+
+            foreach (SchoolAdmin admin in schoolAdmin)
+            {
+                returnList.Add(new SchoolAdminViewModel(admin));
+            }
+
+            return returnList;
+        }
+        
+    }
+}
