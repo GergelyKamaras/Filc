935f0577c8bd90d48b17be6800cc7ce1ec3b59fb
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-30 10:18:02 +0100

    Feat: Import Routing capabilities

--- Filc/ClientApp/src/index.js
+++ Filc/ClientApp/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
