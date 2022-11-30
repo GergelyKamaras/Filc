12e0e925e762cd7839ed038883097f865e0b8799
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-30 11:00:44 +0100

    Fix: Change base structure

--- Filc/ClientApp/src/index.js
+++ Filc/ClientApp/src/index.js
@@ -1,21 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
-    <App />
-  </BrowserRouter>);
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root"));
 
 
 // If you want to start measuring performance in your app, pass a function
