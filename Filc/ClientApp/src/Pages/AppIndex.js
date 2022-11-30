ce4e0b1352a6f1d4f80b02298f7022e9a8bf050a
Dfulop98 <dobo.fulop@icloud.com>
2022-11-30 09:58:17 +0100

    Style:indexPage

--- Filc/ClientApp/src/Pages/AppIndex.js
+++ Filc/ClientApp/src/Pages/AppIndex.js
@@ -1,12 +1,29 @@
 import React from "react";
+import InnerProfil from "../components/IndexPageComponents/InnerProfil";
+import '../Style/IndexPage/IndexPage.css'
 
 const IndexPage = () => {
     return (
         <>
-        <div>
-            sajt
+            <div className="index-parent">
+                <div className="gov-news">
+                    <div>
+                        sajt
+                    </div>
+                     </div>
+                <div className="profile">
+                    <div className="profile-inner">
+                        <InnerProfil logged={false}/>
+                    </div>
+                     </div>
+                <div className="school-news">
+                    sajt
+                     </div>
+                <div className="profile-functs">
+                    sajt
+                     </div>
 
-        </div>
+            </div>
         </>
     );
 };
