2a17cdf446967247866b95b827b992f945ce494b
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-30 14:58:27 +0100

    Add: LessonViewModelForSchool

---
+++ Filc/Models/EntityViewModels/School/LessonViewModelForSchool.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filc.Models.EntityViewModels.School
+{
+    public class LessonViewModelForSchool
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Subject Subject { get; set; }
+
+    }
+}
