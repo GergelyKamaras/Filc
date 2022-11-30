290a24477effa2d6bfd33e24cefc848769d831b0
Dfulop98 <dobo.fulop@icloud.com>
2022-11-30 23:58:03 +0100

    Add: response check in frontend

--- Filc/ClientApp/src/components/controllers/RegistrationFetch.js
+++ Filc/ClientApp/src/components/controllers/RegistrationFetch.js
@@ -1,12 +1,20 @@
 ﻿import React from 'react'
 
-const RegistrationFetch = async (data) => {
-    const response = await fetch('https://localhost:7014/authentication/register', {
-      method: 'POST',
+const RegistrationFetch = (data) => {
+     fetch ("https://localhost:7014/authentication/register",{
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
-        },
+          'Content-Type': 'application/json'
+      },
       body: JSON.stringify(data)
+    }).then((response) => response.json()).then((result)=>{
+      
+      if(result.message === "SUCCESS"){
+        alert("You are logged in");
+        
+      }else{
+          alert("Plese check your login information")
+      }
   })
 }
 
