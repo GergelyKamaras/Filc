30f2262a67beeb711d3bbeecee016289b42b32bf
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-30 15:55:13 +0100

    Add: method for converting schools to school view models

--- Filc/Services/ModelConverter/ModelConverter.cs
+++ Filc/Services/ModelConverter/ModelConverter.cs
@@ -1,20 +1,24 @@
 ﻿using EFDataAccessLibrary.Models;
+using Filc.Models.EntityViewModels.School;
 using Filc.Models.EntityViewModels.SchoolAdmin;
 
 namespace Filc.Services.ModelConverter
 {
     public static class ModelConverter
     {
-        public static List<SchoolAdminViewModel> MapSchoolAdminToSchoolAdminViewModel(List<SchoolAdmin> schoolAdmin)
+        public static List<SchoolAdminViewModel> MapSchoolAdminToSchoolAdminViewModel(List<SchoolAdmin> schoolAdmins)
         {
             List<SchoolAdminViewModel> schoolAdminList = new List<SchoolAdminViewModel>();
-
-            foreach (SchoolAdmin admin in schoolAdmin)
-            {
-                schoolAdminList.Add(new SchoolAdminViewModel(admin));
-            }
+            schoolAdmins.ForEach(admin => schoolAdminList.Add(new SchoolAdminViewModel(admin)));
 
             return schoolAdminList;
+        }
+
+        public static List<SchoolViewModel> MapSchoolToSchoolViewModel(List<School> schools)
+        {
+            List<SchoolViewModel> schoolList = new List<SchoolViewModel>();
+            schools.ForEach(school => schoolList.Add(new SchoolViewModel(school)));
+            return schoolList;
         }
 
     }
