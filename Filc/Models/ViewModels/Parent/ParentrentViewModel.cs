2a9fae4892af29404ce3309f302d2dd4be03d363
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-12-01 12:24:07 +0100

    Add: missing constructor

--- Filc/Models/ViewModels/Parent/ParentrentViewModel.cs
+++ Filc/Models/ViewModels/Parent/ParentrentViewModel.cs
@@ -8,5 +8,13 @@ namespace Filc.Models.ViewModels.Parent
         public int Id { get; set; }
         public ApplicationUser user { get; set; }
         public List<StudentViewModelShared> Children { get; set; }
+
+        public ParentrentViewModel(EFDataAccessLibrary.Models.Parent parent)
+        {
+            Id = parent.Id;
+            user = parent.user;
+            Children = new List<StudentViewModelShared>();
+            parent.Children.ForEach(child => Children.Add(new StudentViewModelShared(child)));
+        }
     }
 }
