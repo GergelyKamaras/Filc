2a052c37def616c63b74482b133da49770d4c6bb
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-12-01 12:16:45 +0100

    Add: parentviewmodel

---
+++ Filc/Models/ViewModels/Parent/ParentrentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EFDataAccessLibrary.Models;
+using Filc.Models.ViewModels.Shared;
+
+namespace Filc.Models.ViewModels.Parent
+{
+    public class ParentrentViewModel
+    {
+        public int Id { get; set; }
+        public ApplicationUser user { get; set; }
+        public List<StudentViewModelShared> Children { get; set; }
+    }
+}
