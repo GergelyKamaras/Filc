821a8f0bbf3847e8d9741f5b03fad240ebdce097
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 13:47:33 +0100

    Feat: Add correct path to SchoolListFetch

--- Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Controllers/SchoolListFetch.js
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Controllers/SchoolListFetch.js
@@ -1,6 +1,6 @@
 ﻿const SchoolListFetch = async () => {
   const response = await fetch(
-    'https://localhost:7014/api/governmentadmins/', {
+    'https://localhost:7014/api/governmentadmins/schools', {
     method: 'GET'
   }
   );
