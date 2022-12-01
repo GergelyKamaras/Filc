adaa21bd55876eb87b7c1b929f6f750cb0270119
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 13:44:32 +0100

    Fix: Replace elements to correct folders

---
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Controllers/SchoolListFetch.js
@@ -0,0 +1,10 @@
+﻿const SchoolListFetch = async () => {
+  const response = await fetch(
+    'https://localhost:7014/api/governmentadmins/', {
+    method: 'GET'
+  }
+  );
+  return response.json();
+}
+
+export default SchoolListFetch;
