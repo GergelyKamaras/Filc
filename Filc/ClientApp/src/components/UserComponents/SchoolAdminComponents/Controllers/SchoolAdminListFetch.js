16f38bba7d0f3ce2ae3933002ac882b78bf9720b
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 12:18:34 +0100

    Feat: Add SchoolAdminListFetch

---
+++ Filc/ClientApp/src/components/UserComponents/SchoolAdminComponents/Controllers/SchoolAdminListFetch.js
@@ -0,0 +1,13 @@
+﻿const GovAdminListFetch = async (id) => {
+  const response = await fetch(
+    `https://localhost:7014/api/schooladmins/schools/${id}/admins`, {
+    method: 'GET'
+  }
+  );
+  if (!response.ok) {
+    return JSON.stringify({});
+  }
+  return response.json();
+}
+
+export default GovAdminListFetch;
