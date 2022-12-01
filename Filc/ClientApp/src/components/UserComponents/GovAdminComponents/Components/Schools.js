7dc4a309332b1cef251f846184a34d359859601a
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2022-12-01 19:46:01 +0100

    Feat: Add working GetAllSchools Fetch with dynamic ListAllSchools

--- Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/Schools.js
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/Schools.js
@@ -1,16 +1,48 @@
 ﻿import SchoolListFetch from "../Controllers/SchoolListFetch";
+import { useState, useEffect } from 'react'
 
 const Schools = () => {
-  const SchoolList = SchoolListFetch();
+    const [schoolData, setSchoolData] = useState({ schools: [] });
+    const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <>
-      <h1>List of Schools</h1>
-      {SchoolList.length > 0 ? SchoolList.forEach(school => {
-        <p>{school}</p>
-      }) : <p>There isn't any schools to list.</p>}
-    </>
-  );
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const Schools = await SchoolListFetch();
+            setSchoolData({ schools: Schools });
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Schools</h1>
+            {isLoading ? (
+                <div>Loading ...</div>
+            ) : (
+                <ul>
+                    {schoolData.schools.map((school) => (
+                        <div className="school" key={school.id}>
+                            <h3>{school.name}</h3>
+                            <p><strong>Address:</strong> {school.address}</p>
+                            <p><strong>Type: </strong>{school.schoolType}</p>
+                            <p><strong>School Admins: </strong></p>
+                                {school.schoolAdmin.map((admin) => (
+                                <p key={admin.id}> - {admin.firstName} {admin.lastName}</p>
+                            ))}
+                            <p><strong>Number of students: </strong>{school.students.length}</p>
+                            <p>Subjects:</p>
+                            <p>Lessons:</p>
+                            <p>Teachers: </p>
+                            <p>Classes: </p>
+                        </div>
+                    ))}
+                </ul>
+            )}
+        </div>
+
+    );
+}	
 
 export default Schools;
