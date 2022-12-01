37568f8bc9dc95e815929a9a045335ff737fda07
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 20:36:32 +0100

    Fix: Rewrite view structure using hooks

--- Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/SchoolAdmins.js
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/SchoolAdmins.js
@@ -1,16 +1,49 @@
 ﻿import SchoolAdminListFetch from "../Controllers/SchoolAdminListFetch";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const SchoolAdmins = () => {
   const params = useParams();
-  const SchoolAdminList = SchoolAdminListFetch(params.schoolid);
+  const [loading, setLoading] = useState(true);
+  const [schoolAdmins, setSchoolAdmins] = useState(false);
+
+  useEffect(
+    () => {
+      const fetchData = async () => {
+
+        const data = await SchoolAdminListFetch(params.schoolid);
+
+        setSchoolAdmins(data);
+        setLoading(false);
+      }
+      fetchData();
+    }
+  );
 
   return (
     <>
-      <h1>List of School Admins</h1>
-      {SchoolAdminList.length > 0 ? SchoolAdminList.forEach(admin => {
-        <p>{admin}</p>
-      }) : <p>There isn't a school with this id or the school doesn't have an administrator.</p>}
+      {loading ? <h1>Loading Data</h1> 
+        : schoolAdmins ? (
+          <div>
+          <h1>SchoolAdmin Data</h1>
+          {
+            schoolAdmins.map((admin) => (
+              <div key={admin.id}>
+                <h3>{admin.firstName} {admin.lastName}</h3>
+                <p><strong>Birthdate:</strong>{admin.birthdate}</p>
+                <p><strong>Address:</strong>{admin.address}</p>
+              </div>
+              )
+            )
+          }
+          </div>
+      ) : (
+            <div>
+              <h1>Data not found</h1>
+              <p>This school either doesn't have a School Admin or doesn't exist.</p>
+            </div>
+      )
+      }
     </>
   );
 }
