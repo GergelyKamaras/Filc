d348cde7a9483f37f71fc0485594c2a3569feb5f
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 12:24:51 +0100

    Feat: Add SchoolAdmins page

---
+++ Filc/ClientApp/src/components/UserComponents/SchoolAdminComponents/Components/SchoolAdmins.js
@@ -0,0 +1,16 @@
+﻿import SchoolAdminListFetch from "../Controllers/SchoolAdminListFetch";
+
+const SchoolAdmins = () => {
+  const SchoolAdminList = SchoolAdminListFetch();
+
+  return (
+    <>
+      <h1>List of School Admins</h1>
+      {SchoolAdminList.length > 0 ? GovAdminList.forEach(admin => {
+        <p>{admin}</p>
+      }) : <p>There isn't a school with this id or the school doesn't have an administrator.</p>}
+    </>
+  );
+}
+
+export default SchoolAdmins;
