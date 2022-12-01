508957f99c7be2376f4038f857334b0db30ba9e8
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 12:05:37 +0100

    Feat: Add GovAdmin page

---
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/GovAdmin.js
@@ -0,0 +1,14 @@
+﻿import GovAdminListFetchById from "../Controllers/GovAdminListFetchById";
+
+const GovAdmin = (id) => {
+  const RequestedAdmin = GovAdminListFetchById(id); 
+
+  return (
+    <>
+      <h1>Admin Data</h1>
+      {RequestedAdmin.length > 0 ? <p>{RequestedAdmin}</p> : <p>We didn't find a Government Admin with this is.</p> }
+    </>
+  );
+}
+
+export default GovAdmin;
