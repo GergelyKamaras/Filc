8712be01dac1e1e6344134e4cb955eda4c2feddb
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 13:33:47 +0100

    Feat: Add SchoolAdmin page

---
+++ Filc/ClientApp/src/components/UserComponents/SchoolAdminComponents/Components/SchoolAdmin.js
@@ -0,0 +1,16 @@
+﻿import SchoolAdminFetchById from "../Controllers/SchoolAdminFetchById";
+import { useParams } from 'react-router-dom';
+
+const SchoolAdmin = () => {
+  let params = useParams();
+  const RequestedAdmin = SchoolAdminFetchById(params.id);
+
+  return (
+    <>
+      <h1>School Admin Data</h1>
+      {RequestedAdmin.length > 0 ? <p>{RequestedAdmin}</p> : <p>We didn't find a School Admin with this id.</p>}
+    </>
+  );
+}
+
+export default SchoolAdmin;
