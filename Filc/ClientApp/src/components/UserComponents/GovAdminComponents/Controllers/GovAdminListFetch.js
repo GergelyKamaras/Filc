e305258318bc102897cb51e14be796d7c23c25aa
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 10:23:04 +0100

    Feat: Add GovAdminListFetch for fetchin all govadmins

---
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Controllers/GovAdminListFetch.js
@@ -0,0 +1,10 @@
+﻿const GovAdminListFetch = async () => {
+  const response = await fetch(
+    'https://localhost:7014/governmentadmins/', {
+      method: 'GET'
+  }
+  );
+  return response;
+}
+
+export default GovAdminListFetch;
