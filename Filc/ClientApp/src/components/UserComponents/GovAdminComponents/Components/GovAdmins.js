8f99c9842989c38aa7a5946b5bb0c50becd0f01e
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 11:13:43 +0100

    Fix: Change display based on fetch content

--- Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/GovAdmins.js
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Components/GovAdmins.js
@@ -1,14 +1,14 @@
 ﻿import GovAdminListFetch from "../Controllers/GovAdminListFetch";
 
 const GovAdmins = () => {
-  const GovAdminList = JSON.stringify(GovAdminListFetch);
+  const GovAdminList = GovAdminListFetch();
 
   return (
     <>
       <h1>List of Government Admins</h1>
-      {GovAdminList?.forEach(admin => {
+      {GovAdminList.length > 0 ? GovAdminList.forEach(admin => {
         <p>{admin}</p>
-      })}
+      }) : <p>There isn't any admins to list.</p>}
     </>
   );
 }
