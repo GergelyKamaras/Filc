c2a907cc53276dea8fc6b55f62763fe7df2892b0
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 10:53:28 +0100

    Feat: Add fetch gov admin by id

---
+++ Filc/ClientApp/src/components/UserComponents/GovAdminComponents/Controllers/GovAdminListFetchById.js
@@ -0,0 +1,10 @@
+﻿const GovAdminListFetchById = async (id) => {
+  const response = await fetch(
+    `https://localhost:7014/api/governmentadmins/${id}`, {
+    method: 'GET'
+  }
+  );
+  return response;
+}
+
+export default GovAdminListFetchById;
