a80605db5d41347ac5269e861bd56ba359ff22d7
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-01 13:26:21 +0100

    Fix: Change wrong type of const in display

--- Filc/ClientApp/src/components/UserComponents/SchoolAdminComponents/Components/SchoolAdmins.js
+++ Filc/ClientApp/src/components/UserComponents/SchoolAdminComponents/Components/SchoolAdmins.js
@@ -8,7 +8,7 @@ const SchoolAdmins = () => {
   return (
     <>
       <h1>List of School Admins</h1>
-      {SchoolAdminList.length > 0 ? GovAdminList.forEach(admin => {
+      {SchoolAdminList.length > 0 ? SchoolAdminList.forEach(admin => {
         <p>{admin}</p>
       }) : <p>There isn't a school with this id or the school doesn't have an administrator.</p>}
     </>
