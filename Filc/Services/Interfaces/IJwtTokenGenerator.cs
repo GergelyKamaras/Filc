e2d77367c02ed4e76b6a623b64521fe52b07cdbf
Dfulop98 <dobo.fulop@icloud.com>
2022-12-13 13:57:42 +0100

    Feat: Jwt token generator inteface, and service from login controller

---
+++ Filc/Services/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Filc.Services.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        public JwtSecurityToken GetToken(List<Claim> authClaims);
+    }
+}
