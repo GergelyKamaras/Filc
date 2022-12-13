4ea78ef7361888ccac53bf5fbaae1c24f8df471a
Dfulop98 <dobo.fulop@icloud.com>
2022-12-13 13:58:03 +0100

    Feat: login interface and service from login controller

---
+++ Filc/Services/Interfaces/ILogin.cs
@@ -0,0 +1,11 @@
+﻿using Filc.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Filc.Services.Interfaces
+{
+    public interface ILogin
+    {
+        public Task<JwtSecurityToken> Login(LoginModel model);
+    }
+}
---
+++ Filc/Services/LoginService.cs
@@ -0,0 +1,47 @@
+﻿using EFDataAccessLibrary.Models;
+using Filc.Models.JWTAuthenticationModel;
+using Filc.Services.Interfaces;
+using Filc.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+
+namespace Filc.Services
+{
+    public class LoginService : ILogin
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public LoginService(UserManager<ApplicationUser> userManager, IJwtTokenGenerator JwtTokenGenerator)
+        {
+            _userManager = userManager;
+            _jwtTokenGenerator = JwtTokenGenerator;
+        }
+        public async Task<JwtSecurityToken> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                return _jwtTokenGenerator.GetToken(authClaims);
+                
+            }
+            throw new Exception();
+        }
+
+
+    }
+}
