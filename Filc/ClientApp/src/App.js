631c929e13f22b3a1ac4331017c7b6becfae0494
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2022-12-14 15:32:15 +0100

    Fix: re-add closing route tags for base route links
    I had removed them by mistake in a prior commit

--- Filc/ClientApp/src/App.js
+++ Filc/ClientApp/src/App.js
@@ -10,7 +10,7 @@ import SchoolAdmins from './Pages/SchoolAdmin/SchoolAdmins';
 import SchoolAdmin from './Pages/SchoolAdmin/SchoolAdmin';
 import Schools from './Pages/Schools/Schools';
 import School from './Pages/Schools/School';
-import IndexPage from './Pages/Home/AppIndex';
+import AppIndex from './Pages/Home/AppIndex';
 import LoginPage from './Pages/Login/LoginPage';
 import RegisterUser from './Pages/Registration/RegisterUser';
 
@@ -19,33 +19,40 @@ function App() {
 
     return (
         <Routes>
-            <Route path="/" element={<Layout />} />
+            <Route path="/" element={<Layout />} >
 
-            {/*Routes available to all users*/}
-            <Route path="" element={<IndexPage/>} />
-            <Route path='/login' element={<LoginPage/>}/>
-            <Route path="Unauthorized" element={<Unauthorized />} />
-            <Route path="schooladmins/:id" element={<SchoolAdmin />} />
+                {/*Routes available to all users*/}
+                <Route path="" element={<AppIndex/>} />
+                <Route path='/login' element={<LoginPage/>}/>
+                <Route path="Unauthorized" element={<Unauthorized />} />
+                <Route path="schooladmins/:id" element={<SchoolAdmin />} />
 
-            {/*TODO: Routes available to Students*/}
-            <Route element={<RequireAuth allowedRoles={["Student"]} />} />
+                {/*TODO: Routes available to Students*/}
+                <Route element={<RequireAuth allowedRoles={["Student"]} />} >
+                    {/* Routes indented here */}
+                </Route>
           
-            {/*TODO: Routes available to Teachers*/}
-            <Route element={<RequireAuth allowedRoles={["Teacher"]} />} />
+                {/*TODO: Routes available to Teachers*/}
+                <Route element={<RequireAuth allowedRoles={["Teacher"]} />} >
+                    {/* Routes indented here */}
+                </Route>
           
-            {/*TODO: Routes available to SchoolAdmins*/}
-            <Route element={<RequireAuth allowedRoles={["SchoolAdmin"]} />} />
-            <Route path="register" element={<RegisterUser />} />
+                {/*TODO: Routes available to SchoolAdmins*/}
+                <Route path="schooladmin" element={<RequireAuth allowedRoles={["SchoolAdmin"]} />} >
+                    <Route path="register" element={<RegisterUser />} />
+                </ Route>
 
-            {/*TODO: Routes available to GovAdmins*/}
-            <Route path="govadmin" element={<RequireAuth allowedRoles={["GovAdmin"]} />} />
-            <Route path="schools/:schoolid/admins" element={<SchoolAdmins />} />
-            <Route path="schooladmins/:id" element={<SchoolAdmin />} />
-            <Route path="schools/" element={<Schools />} />
-            <Route path="schools/:id" element={<School />} />
+                {/*TODO: Routes available to GovAdmins*/}
+                <Route path="govadmin" element={<RequireAuth allowedRoles={["GovAdmin"]} />} >
+                    <Route path="schools/:schoolid/admins" element={<SchoolAdmins />} />
+                    <Route path="schooladmins/:id" element={<SchoolAdmin />} />
+                    <Route path="schools/" element={<Schools />} />
+                    <Route path="schools/:id" element={<School />} />
+                </ Route>
     
-            {/*Not Existing Route*/}
-            <Route path="*" element={<NotFound />} />
+                {/*Not Existing Route*/}
+                <Route path="*" element={<NotFound />} />
+            </ Route>
       </Routes>
     );
 }
