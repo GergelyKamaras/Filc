86d648a97893719217bee788d2f082301e0f39b1
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-15 11:48:15 +0100

    Feat: Add Marks page dummy content

--- Filc/ClientApp/src/Pages/Students/Marks.js
+++ Filc/ClientApp/src/Pages/Students/Marks.js
@@ -1,5 +1,34 @@
-﻿const Marks = () => {
-    return ();
+﻿import { Link } from 'react-router-dom';
+
+const Marks = () => {
+    return (
+        <div>
+            <h1>[BETA] Your grades</h1>
+            <p>Hi! This is the grades page. It's still under development since we don't have data in our database to display.</p><br />
+            <p>This is how it will look like in the future with live data:</p>
+
+            <table>
+                <thead>
+                    <th>Subject</th>
+                    <th>Grades</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Math</td>
+                        <td><Link to="marks/id">5</Link>, <Link to="marks/id">3</Link>, <Link to="marks/id">5</Link></td>
+                    </tr>
+                    <tr>
+                        <td>Literature</td>
+                        <td><Link to="marks/id">3</Link>, <Link to="marks/id">3</Link>, <Link to="marks/id">1</Link></td>
+                    </tr>
+                    <tr>
+                        <td>P.E.</td>
+                        <td><Link to="marks/id">5</Link>, <Link to="marks/id">5</Link>, <Link to="marks/id">5</Link></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Marks;
