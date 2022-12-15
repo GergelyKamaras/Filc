7909cff57574fd6c92ed408d019f86ed1bffe9e0
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-15 09:41:40 +0100

    Feat: Add School Fetch by Id on correct route

---
+++ Filc/ClientApp/src/Pages/Students/FetchSchoolByIdOnStudentRoute.js
@@ -0,0 +1,14 @@
+﻿const FetchSchoolByIdOnStudentRoute = async () => {
+    const id = await jwt(localStorage?.AccessToken)["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+    const response = await fetch(
+        `https://localhost:7014/api/students/schools/${id}`, {
+        method: 'GET'
+    }
+    );
+    if (!response.ok) {
+        return false;
+    }
+    return response.json();
+}
+
+export default FetchSchoolByIdOnStudentRoute;
