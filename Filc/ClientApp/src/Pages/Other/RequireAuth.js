821744c85c1a3af9f20d99467e4af61bfa546cdd
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-15 08:14:49 +0100

    Fix: Change routing logic

--- Filc/ClientApp/src/Pages/Other/RequireAuth.js
+++ Filc/ClientApp/src/Pages/Other/RequireAuth.js
@@ -4,12 +4,12 @@ import jwt from 'jwt-decode';
 const RequireAuth = ({allowedRoles}) => {
   const location = useLocation();
 
-  return (
-    allowedRoles.includes(jwt(localStorage?.AccessToken)["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"])
-      ? <Outlet />
-      : localStorage?.AccessToken
-        ? <Navigate to="/Unauthorized" state={{ from: location }} replace />
-        : <Navigate to="/login" state={{ from: location }} replace />
+    return (
+        localStorage?.AccessToken
+            ? allowedRoles.includes(jwt(localStorage?.AccessToken)["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"])
+                ? <Outlet />
+                : <Navigate to="/Unauthorized" state={{ from: location }} replace />
+            : <Navigate to="/" state={{ from: location }} replace />
   );
 }
 
