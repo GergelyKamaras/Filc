98bd64e05a81ceb3070e7960244f67fa29e50730
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-15 11:55:17 +0100

    Feat: Add Mark page with dummy data

---
+++ Filc/ClientApp/src/Pages/Students/Mark.js
@@ -0,0 +1,20 @@
+﻿const Mark = () => {
+    return (
+        <div>
+            <h1>[BETA] Your grade</h1>
+            <p>Hi! This is the grade page. It's still under development since we don't have data in our database to display.</p><br />
+            <p>This is how it will look like in the future with live data:</p>
+
+            <h3>&lt;Grade&gt;</h3>
+            <ul>
+                <li><strong>Subject: </strong> &lt;Subject&gt;</li>
+                <li><strong>Teacher: </strong> &lt;Teacher&gt;</li>
+                <li><strong>Date: </strong> &lt;Date&gt;</li>
+            </ul>
+            <p>Description:</p><br />
+            <small>&lt;Description&gt;</small>
+        </div>
+    );
+}
+
+export default Mark;
