27b019a46e6269f4bd8437b1e19bd3bfb9d679ed
ReviKZ <kovacs.zoard.1@gmail.com>
2022-12-15 10:47:58 +0100

    Feat: Add Authorization to header

--- Filc/ClientApp/src/Pages/SchoolAdmin/FetchSchoolAdminList.js
+++ Filc/ClientApp/src/Pages/SchoolAdmin/FetchSchoolAdminList.js
@@ -1,7 +1,10 @@
 ﻿const SchoolAdminListFetch = async (id) => {
   const response = await fetch(
     `https://localhost:7014/api/schooladmins/schools/${id}/admins`, {
-    method: 'GET'
+          method: 'GET',
+          headers: {
+              "Authorization": "Bearer" + localStorage.getItem("AccessToken")
+          }
   }
   );
   if (!response.ok) {
