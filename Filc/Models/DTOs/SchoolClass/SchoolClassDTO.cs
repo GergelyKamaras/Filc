b0f983a2293bdc3e07f698f095729b805079f382
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2023-01-09 14:10:47 +0100

    Fix: remove copied dupliace 'SchoolClass' class braces

--- Filc/Models/DTOs/SchoolClass/SchoolClassDTO.cs
+++ Filc/Models/DTOs/SchoolClass/SchoolClassDTO.cs
@@ -6,12 +6,10 @@ namespace Filc.Models.ViewModels.SchoolClass
 {
     public class SchoolClassDTO
     {
-        public class SchoolClass
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public List<StudentSharedDTO>? Students { get; set; }
-            public SchoolSharedDTO? School { get; set; }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<StudentSharedDTO>? Students { get; set; }
+        public SchoolSharedDTO? School { get; set; }
+
     }
 }
