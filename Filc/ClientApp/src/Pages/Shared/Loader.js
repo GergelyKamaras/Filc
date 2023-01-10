2248826b7371071297eb8d3c830dffd4a755beba
Dfulop98 <dobo.fulop@icloud.com>
2023-01-10 22:55:11 +0100

    Feat: loading animation

---
+++ Filc/ClientApp/src/Pages/Shared/Loader.js
@@ -0,0 +1,34 @@
+import "../../Style/Shared/Loading.css"
+
+const Loading = () => {
+    return(
+        <div class="boxes">
+            <div class="box">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="box">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="box">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="box">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading;
