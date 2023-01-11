0e9d49258785286b1ba38bbf9281d7dd1b1e2e63
Dfulop98 <dobo.fulop@icloud.com>
2023-01-11 15:14:40 +0100

    Feat: add css for registration form jsx

--- Filc/ClientApp/src/Pages/Registration/RegistrationForm.js
+++ Filc/ClientApp/src/Pages/Registration/RegistrationForm.js
@@ -1,7 +1,10 @@
 ﻿import FetchSchoolList from "../Schools/FetchSchoolList";
 import RegisterParent  from "./RegisterParent";
 import { useState, useEffect } from 'react';
-
+import {IoMailSharp} from "react-icons/io5"
+import {MdPassword, MdSchool} from "react-icons/md" 
+import {GrDocumentUser} from "react-icons/gr"
+import "../../Style/UserReg/RegisterUser.css"
 
 const RegistrationForm = ({ role, form, updateForm, handleSubmit }) => {
 
@@ -37,43 +40,75 @@ const RegistrationForm = ({ role, form, updateForm, handleSubmit }) => {
 
     return (
          <form>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input defaultValue={form?.email ? form.email : ""} onChange={(e) => updateField(e.target.value, "email")} id="email" type="email" className="form-control" />
+            <div className="register-body">
+                <div className="icon-div">
+                    <IoMailSharp className="form-icon"/>
+                </div>
+                <div className="title-box">
+                     <p> Email: </p>
+                </div>
+                <input defaultValue={form?.email ? form.email : ""} onChange={(e) => updateField(e.target.value, "email")} id="email" type="email" className="input" />
             </div>
-            <div>
-                <label htmlFor="Password">Password</label>
-                <input defaultValue={form?.password ? form.password : ""} onChange={(e) => updateField(e.target.value, "password")} id="Password" type="password" className="form-control" />
+            <div className="register-body">
+            <div className="icon-div">
+                    <MdPassword className="form-icon"/>
+                </div>
+                <div className="title-box">
+                     <p> Password: </p>
+                </div>
+                <input defaultValue={form?.password ? form.password : ""} onChange={(e) => updateField(e.target.value, "password")} id="Password" type="password" className="input" />
             </div>
             {(role === "Student" || role === "Teacher" || role === "SchoolAdmin") &&
                  <div>
-                    <div>
-                        <label htmlFor="FirstName">First Name</label>
-                        <input defaultValue={form?.firstName ? form.firstName : ""} onChange={(e) => updateField(e.target.value, "firstName")} id="FirstName" type="text" className="form-control" />
+                    <div className="register-body">
+                        <div className="icon-div">
+                        <GrDocumentUser className="form-icon"/>
+                        </div>
+                        <div className="title-box">
+                            <p> First Name: </p>
+                        </div>
+                        <input defaultValue={form?.firstName ? form.firstName : ""} onChange={(e) => updateField(e.target.value, "firstName")} id="FirstName" type="text" className="input" />
                     </div>
             
-                    <div>
-                        <label htmlFor="LastName">Last Name</label>
-                        <input defaultValue={form?.lastName ? form.lastName : ""} onChange={(e) => updateField(e.target.value, "lastName")} id="LastName" type="text" className="form-control" />
+                    <div className="register-body">
+                        <div className="icon-div">
+                        </div>
+                        <div className="title-box">
+                            <p> Last Name: </p>
+                        </div>
+                        <input defaultValue={form?.lastName ? form.lastName : ""} onChange={(e) => updateField(e.target.value, "lastName")} id="LastName" type="text" className="input" />
                     </div>
             
-                    <div>
-                        <label htmlFor="BirthDate">Date of Birth</label>
-                        <input defaultValue={form?.birthDate ? form.birthDate : ""} onChange={(e) => updateField(e.target.value, "birthDate")} id="BirthDate" type="date" className="form-control" />
+                    <div className="register-body">
+                        <div className="icon-div">
+                        </div>
+                        <div className="title-box">
+                            <p> Birth Date: </p>
+                        </div>
+                        <input defaultValue={form?.birthDate ? form.birthDate : ""} onChange={(e) => updateField(e.target.value, "birthDate")} id="BirthDate" type="date" className="role-select input" />
                     </div>
 
-                    <div>
-                        <label htmlFor="Address">Address</label>
-                        <input defaultValue={form?.address ? form.address : ""} onChange={(e) => updateField(e.target.value, "address")} id="Address" type="text" className="form-control" />
+                    <div className="register-body">
+                        <div className="icon-div">
+                        </div>
+                        <div className="title-box">
+                            <p> Address: </p>
+                        </div>
+                        <input defaultValue={form?.address ? form.address : ""} onChange={(e) => updateField(e.target.value, "address")} id="Address" type="text" className="input" />
                     </div>
             
-                    <div>
-                        <label htmlFor="SchoolId">School</label>
-                        <select defaultValue={form?.school ? form.school : ""} onChange={(e) => updateField(e.target.value, "SchoolId")} id="Id" type="text" className="form-control">
+                    <div className="register-body">
+                        <div className="icon-div">
+                        <MdSchool className="form-icon"/>
+                        </div>
+                        <div className="title-box">
+                            <p> School: </p>
+                        </div>
+                        <select defaultValue={form?.school ? form.school : ""} onChange={(e) => updateField(e.target.value, "SchoolId")} id="Id" type="text" className="role-select">
                         {isLoading ? (
                             <option>Loading...</option>) :
                             (schoolData.schools.map((school) => (
-                            <option value={school.id} className="school" key={school.id}>{school.name}</option>)))}
+                            <option value={school.id} key={school.id}>{school.name}</option>)))}
                         </select>
                     </div>
                  </div>
@@ -82,7 +117,7 @@ const RegistrationForm = ({ role, form, updateForm, handleSubmit }) => {
                 <RegisterParent updateField={updateField} />
             }
 
-            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Add</button>
+            <button type="submit" className="submit-button" onClick={handleSubmit}>Add</button>
         </form>
 
     )
