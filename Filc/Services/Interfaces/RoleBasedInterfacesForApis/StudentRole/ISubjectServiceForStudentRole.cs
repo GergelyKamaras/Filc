b2a4c1d44096f194b9a7db90b0cdfc12c4af9ffe
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2023-01-13 00:10:49 +0100

    Feat: Create Interfaces for SubjectQueryTableService (non-existing before), for schooladmin and student roles as well.
    Add JWTAuthenticationResponse to AddSubject service method.

---
+++ Filc/Services/Interfaces/RoleBasedInterfacesForApis/StudentRole/ISubjectServiceForStudentRole.cs
@@ -0,0 +1,14 @@
+﻿using EFDataAccessLibrary.Models;
+using Filc.Models.EntityViewModels.Subject;
+
+namespace Filc.Services.Interfaces.RoleBasedInterfacesForApis.StudentRole
+{
+    public interface ISubjectServiceForStudentRole
+    {
+        public Subject GetSubject(int id);
+        public List<SubjectDTO> GetSubjectsByStudent(int studentId);
+
+        public List<Subject> GetSubjectsBySchool(int schoolId);
+    }
+}
+
