2a2b639a941bc329aebb67f781479c21514df196
Dfulop98 <dobo.fulop@icloud.com>
2023-01-12 19:32:04 +0100

    Feat: 401 error page

--- Filc/ClientApp/src/Pages/Other/Unauthorized.js
+++ Filc/ClientApp/src/Pages/Other/Unauthorized.js
@@ -1,10 +1,38 @@
-﻿const Unauthorized = () => {
+﻿import "../../Style/Other/404page.css"
+
+const Unauthorized = () => {
+
   return (
     <>
-      <h1>401</h1>
-      <h2>You are not authorized to access this page.</h2>
-    </>  
-  );
+      <div className="center">
+        <div className="error">
+          <div className="number">4</div>
+          <div className="illustration">
+            <div className="circle" />
+            <div className="clip">
+              <div className="paper">
+                <div className="face">
+                  <div className="eyes">
+                    <div className="eye eye-left" />
+                    <div className="eye eye-right" />
+                  </div>
+                  <div className="rosyCheeks rosyCheeks-left" />
+                  <div className="rosyCheeks rosyCheeks-right" />
+                  <div className="mouth" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="number">1</div>
+        </div>
+        <div className="text">Oops. You dont have a perminission!</div>
+        <a className="button" href="/">
+          Turn back
+        </a>
+      </div>
+    </>
+  )
 }
+
 
 export default Unauthorized;
