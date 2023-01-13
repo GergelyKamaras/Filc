7a76152b14216bcd9e15bdd4b4e246e5fb9dfd64
Dfulop98 <dobo.fulop@icloud.com>
2023-01-13 01:46:48 +0100

    Refactor: add icons and correct a few thinks in the html

--- Filc/ClientApp/src/Pages/Lessons/AddLesson.js
+++ Filc/ClientApp/src/Pages/Lessons/AddLesson.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import "../../Style/UserReg/RegisterUser.css"
 import {FaSchool} from "react-icons/fa"
-import { ImLocation2 } from "react-icons/im"
-import {TbListDetails} from "react-icons/tb"
+import { BsBookFill} from "react-icons/bs"
 import UniversalFetch from '../Shared/FetchUniversal'
 import jwt from 'jwt-decode';
 
@@ -45,46 +44,44 @@ function AddLesson() {
     }
 
     return (
-        <div className='register-form-body'>
+        <div className='register-form'>
             <div className='register-form-header'>
                 <p>Register a new lesson</p>
             </div>
-                    <form>
-                        <div className="register-body">
-                            <div className="icon-div">
-                                <FaSchool className='form-icon'/>
-                            </div>
-                            <div className="title-box">
-                                <p> Name: </p>
-                            </div>
-                            <div>
-                                <input  onChange={(e) => updateField(e.target.value, "Name")} id="Name" type="text" className="input" />
-                            </div>
-                            <div className="icon-div">
-                                <FaSchool className='form-icon'/>
-                            </div>
-                            <div className="title-box">
-                                <p>Subject:</p>
-                            </div>
-                            <div>
-                                <select defaultValue="" onChange={(e) => setLesson(e.target.value)} className="input">
-                                    {isLoading ? (
-                                        <option>Loading subjects...</option>
-                                        ) : ((school.Subjects ?
-                                            school.Subjects.length > 0 ?
-                                                school.Subjects.map((subject) =>
-                                            <option key={subject.id} value={subject.id}>{subject.name}</option>
-                                                ) : <option>There aren't any subjects registered in your school.</option>
-                                            : <option>Something went wrong.</option>))}
-                                </select>
-                            </div>
-                            <div>
-                            <button type="submit" className="submit-button"
-                                onClick={addLessonToDb}>Add new Lesson</button>
-                            </div>
+            <div className='register-form-body'>
+                <form>
+                    <div className="register-body">
+                        <div className="icon-div">
+                            <FaSchool className='form-icon'/>
                         </div>
-                    </form>
-                </div>
+                        <div className="title-box">
+                            <p> Name: </p>
+                        </div>
+                        <input  onChange={(e) => updateField(e.target.value, "Name")} id="Name" type="text" className="input" />
+                    </div>
+                    <div className='register-body'>
+                        <div className="icon-div">
+                            <BsBookFill className='form-icon'/>
+                        </div>
+                        <div className="title-box">
+                            <p>Subject:</p>
+                        </div>
+                        <select defaultValue="" onChange={(e) => setLesson(e.target.value)} className="select">
+                            {isLoading ? (
+                                <option>Loading subjects...</option>
+                                ) : ((school.Subjects ?
+                                    school.Subjects.length > 0 ?
+                                        school.Subjects.map((subject) =>
+                                    <option key={subject.id} value={subject.id}>{subject.name}</option>
+                                        ) : <option>There aren't any subjects registered in your school.</option>
+                                    : <option>Something went wrong.</option>))}
+                        </select>
+                    </div>
+                    <button type="submit" className="submit-button"
+                        onClick={addLessonToDb}>Add new Lesson</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
