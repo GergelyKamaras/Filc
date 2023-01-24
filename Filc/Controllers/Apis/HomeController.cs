104ab4a33aaab5093d6ae4ec6f39e0377d7d311e
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-01-24 12:37:22 +0100

    Add: inmemory database and failed docker attempt

---
+++ Filc/Controllers/Apis/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filc.Controllers.Apis
+{
+    [Route("/api/hello")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, I am Filc!";
+        }
+    }
+}
