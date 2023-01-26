89e58a12a8456189897716951c975293e2be19dd
Dfulop98 <dobo.fulop@icloud.com>
2023-01-26 14:36:35 +0100

    Update: set up a proxy for a deployment server

--- Filc/ClientApp/src/setupProxy.js
+++ Filc/ClientApp/src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:3616';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://172.104.238.6:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://172.104.238.6:3616';
 
 const context =  [
     "/weatherforecast",
